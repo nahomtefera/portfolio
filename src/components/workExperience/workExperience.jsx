58090f76f785de24fd2440450a9e40e6abda31a6
nahom <nahumtefera@gmail.com>
2018-03-19 21:01:19 -0600

    added job object to state, mapped through jobs and printed them, added functionality to show jobs one by one

--- src/components/workExperience/workExperience.jsx
+++ src/components/workExperience/workExperience.jsx
@@ -6,20 +6,91 @@ import './workExperience.css';
 
 class WorkExperience extends Component {
 
+    constructor(props){
+        super(props)
+
+        this.state= {
+            jobs: [ 
+                {
+                    key: 1,
+                    title: "Data Collection Researcher",
+                    company: "Amazon via Nytec",
+                    date: "April-2017 / March-2018",
+                    visible: true,
+                    description: ["Implemented validation tools using HTML, CSS and Javascript to capture errors in data collection materials before sending them to support engineers.", "Collaborating with program managers to develop existing protocol for future data collection.", "Worked abroad in an international project for 3 months as a Data Collection Technician.", "Helped the data collection research team troubleshooting devices, network and hardware issues.", " Managing data collection processes including securing testing materials, handling logistics, training and monitoring participants.", "Localization" ]
+                },
+                {
+                    key: 2,
+                    title: "Maps Quality Analyst",
+                    company: "Apple via Welocalize",
+                    date: "April-2017 / March-2018",
+                    visible: false,
+                    description: ["I have to ensure the quality of the results that a major Maps provider gives to users in Spain's market. My job is to do a research on the results given to the users and ensure that the information is correct." ]
+                },
+                {
+                    key: 3,
+                    title: "Technical Support",
+                    company: "Playstation via SYKES",
+                    date: "April-2017 / March-2018",
+                    visible: false,
+                    description: ["I've been working as Phone Agent, Chat Agent and Escalation Team Agent helping PlayStation consumers on different issues such as account recoveries, billing issues, hardware and networking issues, and more."]
+                }
+            ]
+        }
+
+        this.showJob = this.showJob.bind(this);
+    }
+
+    showJob(event){
+        let prevState = this.state.jobs;
+
+        for(var i=0; i<prevState.length; i++){
+
+            if(prevState[i].key == event.target.id) {
+                prevState[i].visible = true;
+            }else{
+                prevState[i].visible = false;
+            }
+        }
+
+        this.setState({
+            jobs: prevState
+        })
+    }
+
     render() {
         return (
             <div className="work-experience-container">
-                <div className="left-arrow arrow-icon-container">
+                {/* <div className="left-arrow arrow-icon-container">
                     <img className="arrow-icon" src={arrowBack} alt="arrow Back"/>
-                </div>
+                </div> */}
 
                 <div className="work-list">
-                    First work exp
+                    {this.state.jobs.map(job=>{
+                        return(
+                            <div key={job.key} className={job.visible ? "job-container" : "job-container hide"}>
+                                <h4 className="job-title">{job.title}</h4>
+                                <h4 className="job-company">{job.company}</h4>
+                                <ul>
+                                    {job.description.map(item=>{
+                                        return <li> {item} </li>
+                                    })}
+                                </ul>
+                            </div>
+                        )
+                    })}
+                    <div className="work-nav">
+                        {this.state.jobs.map(job=>{
+                            return(
+                                <span id={job.key} className="work-nav-icon" onClick={this.showJob} key={job.key}>○</span>
+                            )
+                        })}
+                    </div>
                 </div>
 
-                <div className="right-arrow arrow-icon-container">
+                {/* <div className="right-arrow arrow-icon-container">
                     <img className="arrow-icon" src={arrowForward} alt="arrow Forward"/>
-                </div>
+                </div> */}
             </div>
         )
     }
