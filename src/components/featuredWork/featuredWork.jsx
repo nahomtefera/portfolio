9e7330a9f71ad5fa0c8d93bac1010c28e8d29887
Nahum T <nahumtefera@gmail.com>
2018-03-26 09:10:32 -0700

    changed mockups to mp4 files

--- src/components/featuredWork/featuredWork.jsx
+++ src/components/featuredWork/featuredWork.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
+import FontAwesome from 'react-fontawesome';
 import './featuredWork.css';
 
 Modal.setAppElement('#root')
@@ -12,7 +13,10 @@ const customStyles = {
       right                 : 'auto',
       bottom                : 'auto',
       marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)'
+      transform             : 'translate(-50%, -50%)',
+      maxWidth              : "85%",
+      width                 : "740px",
+      overflow              : "hidden"
     }
   };
   
@@ -29,64 +33,72 @@ class FeaturedWork extends Component {
                 {
                     id: Math.random(),
                     title: "Bravoresume",
+                    db_title: "bravoresume",
                     description: "Bravoresume is a web app that I created that help users to create and download customized resumes. It was built on React and pdfMake.",
-                    images: require("../../images/videos/coffeeshop.mp4"),
-                    mockup: require('../../images/mockups/bravoresume.png'),
+                    images: [require("../../images/featuredWork/bravo/bravo-mobile.png")],
+                    mockup: require('../../images/videos/mockupvideos/bravoresume.mp4'),
                     modalIsOpen: false,
                 },
                 {
                     id: Math.random(),
                     title: "San Francisco Coffee Shop Finder",
+                    db_title: "sfcoffeeshopfinder",
                     description: "This web app connects to Foursquare API to get a list of the Coffee Shops in San Francisco with the best ratings and displays them on a map, powered by Google Maps API.",
-                    mockup: require('../../images/mockups/sfcoffeefinder.png'),
-                    images: require("../../images/videos/todoapp.mp4"),
+                    mockup: require('../../images/videos/mockupvideos/sfcoffeefinder.mp4'),
+                    images: [require("../../images/videos/todoapp.mp4")],
                     modalIsOpen: false,
                 },
                 {
                     id: Math.random(),
                     title: "Textiles Europeos",
+                    db_title: "textileseuropeos",
                     description: "I worked with a textile company in Spain to help them improve their UI. I wanted to give the website a cleaner appereance, update their old logos and icons and make it easier to navigate.",
-                    mockup: require('../../images/mockups/textileseuropeos.png'),
+                    mockup: require('../../images/videos/mockupvideos/textileseuropeos.mp4'),
                     images: [],
                     modalIsOpen: false,
                 },
                 {
                     id: Math.random(),
                     title: "Made in Peru Journal",
+                    db_title: "madeinperujournal",
                     description: "I worked with a graphic designer to create the landing page of a journal that she created.",
-                    mockup: require('../../images/mockups/madeinperujournal.png'),
+                    mockup: require('../../images/videos/mockupvideos/madeinperujournal.mp4'),
                     images: [],
                     modalIsOpen: false,
                 },
                 {
                     id: Math.random(),
                     title: "My First Portfolio",
+                    db_title: "portfoliotemplate",
                     description: "",
-                    mockup: require('../../images/mockups/firstportfolio.png'),
+                    mockup: require('../../images/videos/mockupvideos/portfoliotemplate.mp4'),
                     images: [],
                     modalIsOpen: false,
                 },
                 {
                     id: Math.random(),
                     title: "Asana Tasks",
+                    db_title: "asanapp",
                     description: "This web app connects to Asana's API. It retrieves projects and apps.",
-                    mockup: require('../../images/mockups/asanapp.png'),
+                    mockup: require('../../images/videos/mockupvideos/asanapp.mp4'),
                     images: [],
                     modalIsOpen: false,
                 },
                 {
                     id: Math.random(),
                     title: "Todo App with Firebase",
+                    db_title: "todoapp",
                     description: "",
-                    mockup: require('../../images/mockups/todoapp.png'),
+                    mockup: require('../../images/videos/mockupvideos/todoapp.mp4'),
                     images: [],
                     modalIsOpen: false,
                 },
                 {
                     id: Math.random(),
                     title: "Design Studio",
+                    db_title: "designstudio",
                     description: "",
-                    mockup: require('../../images/mockups/designstudio.png'),
+                    mockup: require('../../images/videos/mockupvideos/designstudio.mp4'),
                     images: [],
                     modalIsOpen: false,
                 },  
@@ -95,7 +107,14 @@ class FeaturedWork extends Component {
     
         this.openModal = this.openModal.bind(this);
         this.closeModal = this.closeModal.bind(this);
+        this.playVideo = this.playVideo.bind(this);
       }
+
+    componentDidMount(){
+        for (var ref in this.refs) {
+            this.refs[ref].click()
+        }
+    }
 
     openModal(event) {
         let prevState = this.state.projects;
@@ -110,6 +129,13 @@ class FeaturedWork extends Component {
             modalIsOpen: true,
             projects: prevState
         })
+    }
+
+    playVideo(event) {
+        let video = event.target;
+        
+        video.play()
+        console.log(event.target);
     }
 
     closeModal() {
@@ -131,13 +157,18 @@ class FeaturedWork extends Component {
                 <div className="featured-work">
                     {this.state.projects.map(project=>{
                         return(
-                            <img 
-                            id={project.id} 
-                            src={project.mockup}
-                            className="mockup-img" 
-                            alt="project mockkup" 
-                            onClick={this.openModal} 
-                        />
+                            // <img 
+                            // id={project.id} 
+                            // src={project.mockup}
+                            // className="mockup-img" 
+                            // alt="project mockkup" 
+                            // onClick={this.openModal} 
+                            // />
+
+                            <video className="mockup-img" onClick={this.playVideo} ref={"mockup_gif_"+project.db_title}> 
+                                <source src={project.mockup} type="video/mp4"/>
+                                {/* <img src={project.mockup} /> */}
+                            </video>
                         )
                     })}
                 </div>
@@ -153,14 +184,30 @@ class FeaturedWork extends Component {
                                 style={customStyles}
                                 contentLabel="Example Modal"
                             >
-
-                                <h2 ref={subtitle => this.subtitle = subtitle}>{project.title}</h2>
-                                <button onClick={this.closeModal}>close</button>
+                            
+                                <FontAwesome 
+                                    className="close-modal-icon" 
+                                    onClick={this.closeModal}
+                                    name='times'
+                                    size='2x'
+                                    style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                                />      
+                                <h2 className="modal-title">{project.title}</h2>
                                 
-
-                                <video autoplay="autoplay" loop="loop" width="400" height="300">
-                                <source src={project.images} type="video/mp4" />
-                                </video>
+                                <div className="all-images-container">
+                                    <div className="all-images">
+                                        <div className="all-images-scroll-container">
+                                            {project.images.map(picture => {
+                                                return (
+                                                    <div className="project-images-container">
+                                                        <img className="project-images" src={picture} />
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                </div>
+                                
 
                                 <div className="modal-description">{project.description}</div>
 
